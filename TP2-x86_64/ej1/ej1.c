1d7b7d1fd02da6876d43fc2b900986844c137c11
luciaboukay <lboukay@udesa.edu.ar>
2025-04-07 15:25:21 -0300

    Funciones c

--- TP2-x86_64/ej1/ej1.c
+++ TP2-x86_64/ej1/ej1.c
@@ -1,15 +1,67 @@
 #include "ej1.h"
 
 string_proc_list* string_proc_list_create(void){
+	/* Crea una lista vacia */
+	string_proc_list* list = (string_proc_list*)malloc(sizeof(string_proc_list));
+	if(list == NULL){
+		printf("Error: No se pudo crear la lista\n");
+		return NULL;
+	}
+	list->first = NULL;
+	list->last  = NULL;
+	return list;
 }
 
 string_proc_node* string_proc_node_create(uint8_t type, char* hash){
+	string_proc_node* node = (string_proc_node*)malloc(sizeof(string_proc_node));
+	if(node == NULL){
+		printf("Error: No se pudo crear el nodo\n");
+		return NULL;
+	}
+	node->next= NULL;
+	node->previous= NULL;
+	node->type = type;
+	node->hash = hash;
+	return node;
 }
 
 void string_proc_list_add_node(string_proc_list* list, uint8_t type, char* hash){
+	string_proc_node* node = string_proc_node_create(type, hash);
+	if(node == NULL){
+		printf("Error: No se pudo crear el nodo\n");
+		return;
+	}
+	if(list->first == NULL){
+		list->first = node;
+		list->last  = node;
+	}else{
+		list->last->next = node;
+		node->previous   = list->last;
+		list->last      = node;
+	}
 }
 
 char* string_proc_list_concat(string_proc_list* list, uint8_t type , char* hash){
+	/*Genera un nuevo hash concatenando el pasado por parámetro con todos los hashes 
+	de los nodos de la lista cuyos tipos coinciden con el pasado por parámetro 
+	*/
+	string_proc_node* current_node = list->first;
+	char* new_hash = (char*)malloc(1);
+	new_hash[0] = '\0';
+	while(current_node != NULL){
+		if(current_node->type == type){
+			char* temp = str_concat(new_hash, current_node->hash);
+			free(new_hash);
+			new_hash = temp;
+		}
+		current_node = current_node->next;
+	}
+	if(hash != NULL){
+		char* temp = str_concat(hash, new_hash);
+		free(new_hash);
+		new_hash = temp;
+	}
+	return new_hash;
 }
 
 
